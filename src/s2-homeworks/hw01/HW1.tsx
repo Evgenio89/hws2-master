cd152fc82c8f8272317bf4e33a5448c3143ea729
Evgenio89 <evgenio891@gmail.com>
2023-08-27 12:59:18 +0300

    HM2

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -3,7 +3,7 @@ import Message from './message/Message'
 import MessageSender from './message-sender/MessageSender'
 import s2 from '../../s1-main/App.module.css'
 import FriendMessage from './friend-message/FriendMessage'
-import avatar from './avatar.png'
+
 
 /*
 * 1 - описать тип MessageType
@@ -14,14 +14,24 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+    id: number,
+    user: {
+        avatar: string,
+        name: string
+    },
+    message: {
+        text: string,
+        time: string
+    }
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
     id: 0,
     user: {
-        avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        avatar: 'https://static.more.tv/actor/avatar/5feab82564aa7.jpeg', // можно менять
+        name: 'Elon Muck',  // можно менять
     },
     message: {
         text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
@@ -31,8 +41,8 @@ export const message0: MessageType = {
 export const friendMessage0: MessageType = {
     id: 100,
     user: {
-        avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
+        avatar:'https://www.labirint.ru/images/upl/descripts/pic_1478690583.jpg', // можно менять
+        name: 'Donald Trump', // можно менять
     },
     message: {
         text: 'зеркальное сообщение для тренировки css', // можно менять
--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -1,37 +1,43 @@
 import React from 'react'
 import s from './FriendMessage.module.css'
+import {MessagePropsType} from "../message/Message";
 
 // создать тип вместо any и отобразить приходящие данные
-const FriendMessage = (props: any) => {
+const FriendMessage = (props: MessagePropsType) => {
     return (
         <div
             id={'hw1-friend-message-' + props.message.id}
             className={s.friendMessage}
         >
             <div className={s.friendImageAndText}>
-                <img
-                    id={'hw1-friend-avatar-' + props.message.id}
-                    // создаёт студент
-
-                    //
-                />
+                <div>
+                    <img
+                        id={'hw1-friend-avatar-' + props.message.id}
+                        // создаёт студент
+                        src={props.message.user.avatar}
+                        alt={'Tramp'}
+                        //
+                    />
+                </div>
+                <div className={s.corner}>
+                </div>
                 <div className={s.friendText}>
                     <div
                         id={'hw1-friend-name-' + props.message.id}
                         className={s.friendName}
                     >
                         {/*создаёт студент*/}
-
+                        {props.message.user.name}
                         {/**/}
                     </div>
-                    <pre
+                    <div
                         id={'hw1-friend-text-' + props.message.id}
                         className={s.friendMessageText}
                     >
                         {/*создаёт студент*/}
-
+                        {props.message.message.text}
                         {/**/}
-                    </pre>
+                    </div>
                 </div>
             </div>
             <div
@@ -39,7 +45,7 @@ const FriendMessage = (props: any) => {
                 className={s.friendTime}
             >
                 {/*создаёт студент*/}
-
+                {props.message.message.time}
                 {/**/}
             </div>
         </div>
